5d7981543103038980e34c414f5f3c96e5f9c532
akr368 <akr368@nyu.edu>
2016-11-02 23:08:51 -0400

    Upd1

---
+++ Arrays/1A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+void findPairs(int a[],int n,int sum){
+	 
+   
+   int i,j;
+   for(i=0;i<n-1;i++){
+   	 for(j=i+1;j<n;j++){
+   	 	
+   	 	if(a[i]+a[j]==sum){
+   	 		
+   	 		cout<<"Pairs:"<<a[i]<<"+"<<a[j]<<"="<<sum<<endl;
+   	 		break;
+   	 	}
+
+   	 }
+   }
+}
+
+
+int main(){
+
+ int a[]={1,2,3,4,10};
+ int n=sizeof(a)/sizeof(a[0]);
+ int sum=7;
+ findPairs(a,n,sum);
+
+    
+
+}
