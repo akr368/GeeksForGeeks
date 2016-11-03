cf117c55e968d6dcdb1748e7fa291f3e45b77f86
akr368 <akr368@nyu.edu>
2016-11-03 00:53:00 -0400

    Update Problem1

---
+++ Arrays/1C.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#define MAX 100000
+
+using namespace std;
+//This method works if we know the range of numbers
+//beforehand. 
+
+
+void findPair(int a[],int n,int sum){
+
+   bool hashmap[MAX]={0};
+   int i=0;
+   for(i=0;i<n;i++){
+   
+   	 if(hashmap[sum-a[i]]==1){
+   	 	cout<<"Pair:"<<sum-a[i]<<"+"<<a[i]<<endl;
+   	 	return;
+   	 }
+   	 	hashmap[a[i]]=1;
+
+   }
+
+   cout<<"No such pair exists!";
+
+}
+
+int main(){
+
+   int a[]={5,4,3,2,1};
+   int sum=7;
+   int n=sizeof(a)/sizeof(a[0]);
+   findPair(a,n,sum);
+    
+
+}
