fd7290d677e01e19830a2fbe32d54fa7a6aada34
akr368 <akr368@nyu.edu>
2017-02-02 13:57:21 -0500

    Update

---
+++ Trees/14.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+int max(int a,int b) {
+  if(a>b)
+    return a;
+  else 
+    return b;
+}
+
+struct node* newnode(int data){
+
+    struct node* node=(struct node*)malloc(sizeof(struct node));
+    node->data=data;
+    node->left=NULL;
+    node->right=NULL;
+    return node;
+}
+
+
+int size(struct node *node){
+
+  if(node==NULL)
+    return 0;
+  return 1 + size(node->left)+ size(node->right);
+
+}
+
+
+
+int main() { 
+
+    struct node* root=newnode(1);
+    root->left=newnode(2);
+    root->right=newnode(3);
+    root->left->left=newnode(4);
+    root->left->left->left=newnode(10);
+
+    root->left->left->left->left=newnode(12);
+    root->left->right=newnode(5);
+    cout<<size(root)<<endl;
+   
+
+
+    return 0;
+
+}
+
