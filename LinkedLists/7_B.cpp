3214e5a47291b68c528ef4c6b9cf2e8e516f788b
akr368 <akr368@nyu.edu>
2017-02-22 17:57:47 -0500

    upd5

---
+++ LinkedLists/7_B.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+
+void push(struct node **head_ref,int data_item){
+   
+   struct node *newnode=(struct node*)malloc(sizeof(struct node));
+   newnode->data=data_item;
+   newnode->next=*head_ref;
+      *head_ref=newnode;
+
+}
+
+
+int length(struct node *node){
+   
+   if(node==NULL)
+    return 0;
+
+   return 1+length(node->next);
+
+}
+
+
+
+int main(){
+
+	struct node *head=NULL;
+    push(&head,1);
+    push(&head,2);
+    push(&head,3);
+    cout<<"Length : "<<length(head)<<endl;
+
+
+    return 0;
+
+}
